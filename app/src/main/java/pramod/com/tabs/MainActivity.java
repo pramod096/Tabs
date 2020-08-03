a144bd0bc2c538d937f6528401e58d268c6133f5
Pramod Reddy Gonegari <pramodreddy096@gmail.com>
2020-08-03 19:30:23 +0530

    Added Fragment Functionality

--- app/src/main/java/pramod/com/tabs/MainActivity.java
+++ app/src/main/java/pramod/com/tabs/MainActivity.java
@@ -1,14 +1,45 @@
 package pramod.com.tabs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager vp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vp = findViewById(R.id.viewpager);
+    }
+
+    class vpAdapter extends FragmentStatePagerAdapter {
+
+        public vpAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0: return new RedFragment();
+                case 1: return new BlueFragment();
+                case 2: return new YellowFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
     }
 }
