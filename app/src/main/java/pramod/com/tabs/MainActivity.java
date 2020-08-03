d5ab575ecafb371041be2b757fef65df740f5607
Pramod Reddy Gonegari <pramodreddy096@gmail.com>
2020-08-03 19:54:43 +0530

    Added Tab Attribute

--- app/src/main/java/pramod/com/tabs/MainActivity.java
+++ app/src/main/java/pramod/com/tabs/MainActivity.java
@@ -1,6 +1,7 @@
 package pramod.com.tabs;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -9,18 +10,23 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager vp;
+    private TabLayout tl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         vp = findViewById(R.id.viewpager);
+        tl = findViewById(R.id.tablayout);
         vpAdapter adapter = new vpAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
 
         vp.setAdapter(adapter);
+        tl.setupWithViewPager(vp);
     }
 
     class vpAdapter extends FragmentStatePagerAdapter {
@@ -43,6 +49,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             return 3;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch(position){
+                case 0: return "RED";
+                case 1: return "BLUE";
+                case 2: return "YELLOW";
+            }
+            return super.getPageTitle(position);
         }
     }
 }
